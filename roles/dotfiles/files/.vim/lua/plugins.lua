95291775a1609a7a261cb513ec016d0bd6fdb5ab
Amadou Cisse <cisse.amadou.9@gmail.com>
2021-12-28 11:28:46 +0100

    Update

--- roles/dotfiles/files/.vim/lua/plugins.lua
+++ roles/dotfiles/files/.vim/lua/plugins.lua
@@ -125,6 +125,18 @@ require('packer').startup(function(use)
     end
   }
 
+  use({
+    "ethanholz/nvim-lastplace",
+    event = "BufRead",
+    config = function()
+      require("nvim-lastplace").setup({
+        lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+        lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+        lastplace_open_folds = true,
+      })
+    end,
+  })
+
   use {"haringsrob/nvim_context_vt"}
   use { "stevearc/dressing.nvim" }
 
