644e34509f2e5cb037331de188041c5bd1d4dabf
Amadou Cisse <cisse.amadou.9@gmail.com>
2022-02-02 11:34:06 +0100

    Update

--- roles/dotfiles/files/.vim/lua/config/lsp.lua
+++ roles/dotfiles/files/.vim/lua/config/lsp.lua
@@ -78,42 +78,42 @@ local setup = function()
 
     -- nvim-dap
     -- I only use nvim-dap with Scala, so we keep it all in here
-    -- local dap = require("dap")
+    local dap = require("dap")
 
-    -- dap.configurations.scala = {
-    --   {
-    --     type = "scala",
-    --     request = "launch",
-    --     name = "RunOrTest",
-    --     metals = {
-    --       runType = "runOrTestFile",
-    --       --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-    --     },
-    --   },
-    --   {
-    --     type = "scala",
-    --     request = "launch",
-    --     name = "Test Target",
-    --     metals = {
-    --       runType = "testTarget",
-    --     },
-    --   },
-    -- }
+    dap.configurations.scala = {
+      {
+        type = "scala",
+        request = "launch",
+        name = "RunOrTest",
+        metals = {
+          runType = "runOrTestFile",
+          --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+        },
+      },
+      {
+        type = "scala",
+        request = "launch",
+        name = "Test Target",
+        metals = {
+          runType = "testTarget",
+        },
+      },
+    }
 
-    -- map("n", "<leader>dc", [[<cmd>lua require("dap").continue()<CR>]])
-    -- map("n", "<leader>dr", [[<cmd>lua require("dap").repl.toggle()<CR>]])
-    -- map(
-    --   "n",
-    --   "<leader>ds",
-    --   [[<cmd>lua require("dap.ui.widgets").sidebar(require("dap.ui.widgets").scopes).toggle()<CR>]]
-    -- )
-    -- map("n", "<leader>dK", [[<cmd>lua require("dap.ui.widgets").hover()<CR>]])
-    -- map("n", "<leader>dt", [[<cmd>lua require("dap").toggle_breakpoint()<CR>]])
-    -- map("n", "<leader>dso", [[<cmd>lua require("dap").step_over()<CR>]])
-    -- map("n", "<leader>dsi", [[<cmd>lua require("dap").step_into()<CR>]])
-    -- map("n", "<leader>dl", [[<cmd>lua require("dap").run_last()<CR>]])
+    map("n", "<leader>dc", [[<cmd>lua require("dap").continue()<CR>]])
+    map("n", "<leader>dr", [[<cmd>lua require("dap").repl.toggle()<CR>]])
+    map(
+      "n",
+      "<leader>ds",
+      [[<cmd>lua require("dap.ui.widgets").sidebar(require("dap.ui.widgets").scopes).toggle()<CR>]]
+    )
+    map("n", "<leader>dK", [[<cmd>lua require("dap.ui.widgets").hover()<CR>]])
+    map("n", "<leader>dt", [[<cmd>lua require("dap").toggle_breakpoint()<CR>]])
+    map("n", "<leader>dso", [[<cmd>lua require("dap").step_over()<CR>]])
+    map("n", "<leader>dsi", [[<cmd>lua require("dap").step_into()<CR>]])
+    map("n", "<leader>dl", [[<cmd>lua require("dap").run_last()<CR>]])
 
-    -- require("metals").setup_dap()
+    require("metals").setup_dap()
   end
 
   -- sumneko lua
