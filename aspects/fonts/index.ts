08536a1ec701f558cee6125b98372f97ef9789d7
Amadou <cisse.amadou.9@gmail.com>
2022-07-10 18:47:42 +0200

    Update

--- aspects/fonts/index.ts
+++ aspects/fonts/index.ts
@@ -8,31 +8,6 @@ task('create ~/Library/Fonts', async () => {
   });
 });
 
-task('install iosevka', async () => {
-  // Check to see whether submodule is present.
-  const contents = 'vendor/fonts/ttc-iosevka-15';
-
-  const stats = await stat(contents);
-
-  if (stats === null) {
-    await log.warn(`Submodule contents missing at "${contents}"`);
-
-    return await skip();
-  } else if (stats instanceof Error) {
-    throw stats;
-  }
-
-  const files = resource.files('iosevka/*.ttc');
-
-  const target = path.home.join('Library/Fonts');
-
-  for (const ttc of files) {
-    await command('cp', [ttc, target], {
-      creates: target.join(ttc.basename),
-    });
-  }
-});
-
 task('install JetBrains Mono', async () => {
   // Check to see whether submodule is present.
   const contents = 'vendor/fonts/JetBrainsMono-1.0.3/TTF';
@@ -48,31 +23,6 @@ task('install JetBrains Mono', async () => {
   }
 
   const files = resource.files('jetbrains-mono/*.ttf');
-
-  const target = path.home.join('Library/Fonts');
-
-  for (const ttf of files) {
-    await command('cp', [ttf, target], {
-      creates: target.join(ttf.basename),
-    });
-  }
-});
-
-task('install Hack', async () => {
-  // Check to see whether submodule is present.
-  const contents = 'vendor/fonts/Hack';
-
-  const stats = await stat(contents);
-
-  if (stats === null) {
-    await log.warn(`Submodule contents missing at "${contents}"`);
-
-    return await skip();
-  } else if (stats instanceof Error) {
-    throw stats;
-  }
-
-  const files = resource.files('hack-mono/*.ttf');
 
   const target = path.home.join('Library/Fonts');
 
