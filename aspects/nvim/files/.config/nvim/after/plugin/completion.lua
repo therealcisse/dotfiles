8c98bc5c1f4b30ec336f21f09705501f22f236a3
Amadou Cisse <cisse.amadou.9@gmail.com>
2022-07-15 11:58:07 +0200

    Update

--- aspects/nvim/files/.config/nvim/after/plugin/completion.lua
+++ aspects/nvim/files/.config/nvim/after/plugin/completion.lua
@@ -29,11 +29,11 @@ local cmp = require "cmp"
 
 cmp.setup {
   mapping = cmp.mapping.preset.insert({
+    ['<C-e>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   -- mapping = {
