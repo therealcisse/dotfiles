bbb78d5df7ab345ba684e6848edc1c81042dfe3a
Amadou Cisse <cisse.amadou.9@gmail.com>
2022-08-06 07:05:59 +0200

    Update surround.lua

--- aspects/nvim/files/.config/nvim/lua/trc/surround.lua
+++ aspects/nvim/files/.config/nvim/lua/trc/surround.lua
@@ -4,43 +4,6 @@ if not status_ok then
 end
 
 surround.setup({
-    keymaps = { -- vim-surround style keymaps
-        insert = "ys",
-        visual = "S",
-        delete = "ds",
-        change = "cs",
-    },
-    delimiters = {
-        pairs = {
-          ["b"] = { "(", ")" },
-          ["B"] = { "{", "}" },
-          ["("] = { "( ", " )" },
-          [")"] = { "(", ")" },
-          ["{"] = { "{ ", " }" },
-          ["}"] = { "{", "}" },
-          ["<"] = { "< ", " >" },
-          [">"] = { "<", ">" },
-          ["["] = { "[ ", " ]" },
-          ["]"] = { "[", "]" },
-        },
-        separators = {
-            ["'"] = { "'", "'" },
-            ['"'] = { '"', '"' },
-            ["`"] = { "`", "`" },
-        },
-        HTML = {
-            ["t"] = true, -- Use "t" for HTML-style mappings
-        },
-        aliases = {
-            ["a"] = ">", -- Single character aliases apply everywhere
-            ["b"] = ")",
-            ["B"] = "}",
-            ["r"] = "]",
-            ["q"] = { '"', "'", "`" }, -- Table aliases only apply for changes/deletions
-        },
-    },
-    highlight_motion = {
-        duration = 0,
-    }
+
 })
 
