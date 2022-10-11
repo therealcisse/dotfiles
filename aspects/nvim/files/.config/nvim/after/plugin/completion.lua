67695445a79131fe3c0c422cff6d629daed78474
Amadou Cisse <acisse@demystdata.com>
2022-10-11 10:19:27 +0200

    update

--- aspects/nvim/files/.config/nvim/after/plugin/completion.lua
+++ aspects/nvim/files/.config/nvim/after/plugin/completion.lua
@@ -115,7 +115,7 @@ cmp.setup {
   --        priority
   --        max_item_count
   --        (more?)
-  sources = {
+  sources = cmp.config.sources({
     -- { name = "gh_issues" },
 
     -- Youtube: Could enable this only for lua, but nvim_lua handles that already.
@@ -124,37 +124,37 @@ cmp.setup {
     { name = "nvim_lsp" },
     { name = "path" },
     { name = "luasnip" },
-    { name = "buffer", keyword_length = 5 },
-    { name = 'nvim_lsp_signature_help' },
-  },
 
-  sorting = {
-    -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
-    comparators = {
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
+    -- { name = 'nvim_lsp_signature_help' },
+  }, { name = "buffer", keyword_length = 5 }),
 
-      -- copied from cmp-under, but I don't think I need the plugin for this.
-      -- I might add some more of my own.
-      function(entry1, entry2)
-        local _, entry1_under = entry1.completion_item.label:find "^_+"
-        local _, entry2_under = entry2.completion_item.label:find "^_+"
-        entry1_under = entry1_under or 0
-        entry2_under = entry2_under or 0
-        if entry1_under > entry2_under then
-          return false
-        elseif entry1_under < entry2_under then
-          return true
-        end
-      end,
-
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
-    },
-  },
+  -- sorting = {
+  --   -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
+  --   comparators = {
+  --     cmp.config.compare.offset,
+  --     cmp.config.compare.exact,
+  --     cmp.config.compare.score,
+  --
+  --     -- copied from cmp-under, but I don't think I need the plugin for this.
+  --     -- I might add some more of my own.
+  --     function(entry1, entry2)
+  --       local _, entry1_under = entry1.completion_item.label:find "^_+"
+  --       local _, entry2_under = entry2.completion_item.label:find "^_+"
+  --       entry1_under = entry1_under or 0
+  --       entry2_under = entry2_under or 0
+  --       if entry1_under > entry2_under then
+  --         return false
+  --       elseif entry1_under < entry2_under then
+  --         return true
+  --       end
+  --     end,
+  --
+  --     cmp.config.compare.kind,
+  --     cmp.config.compare.sort_text,
+  --     cmp.config.compare.length,
+  --     cmp.config.compare.order,
+  --   },compare.sort_text
+  -- },
 
   -- Youtube: mention that you need a separate snippets plugin
   snippet = {
